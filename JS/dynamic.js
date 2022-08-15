1b556a5bfdf3bdf0091045e9f5fb36c7ea57cebc
faria-r <fariacse11@gmail.com>
2022-08-15 23:53:32 +0600

    added js file

---
+++ JS/dynamic.js
@@ -0,0 +1,4 @@
+const allh2 = document.getElementsByTagName('h2');
+allh2.style.color = 'red';
+document.getElementById('hi').style.color = 'red';
+document.getElementById('hi').style.backgroundColor = 'aqua';
